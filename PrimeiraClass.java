7b96e338c922c5df6cd32e7ef551db38e52bf182
Clasp systems <148411620+danielpaiv@users.noreply.github.com>
2024-02-05 14:10:43 -0300

    Add files via upload

---
+++ PrimeiraClass.java
@@ -0,0 +1,11 @@
+package ProjetoJava.Daniel;
+
+public class PrimeiraClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Olá mundo");
+
+	}
+
+}
